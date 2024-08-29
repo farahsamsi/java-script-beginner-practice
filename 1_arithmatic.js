e052b7ca692b71519154ac1c45504a011080499f
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-29 20:04:14 +0600

    first problem solved

---
+++ 1_arithmatic.js
@@ -0,0 +1,16 @@
+/**
+ * You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result.
+ */
+
+var balance = 1000;
+var priceOrange = 80;
+var priceApple = 100;
+var result = balance - (priceOrange+priceApple)
+
+console.log(result)
