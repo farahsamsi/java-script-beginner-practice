e62881afa8bdd7067290c8341ffed9066b582616
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-29 22:28:23 +0600

    if and if else statements

---
+++ 8_if.js
@@ -0,0 +1,43 @@
+/**
+ * if you buy food
+ *      i will come
+ * 
+ * ------------
+ * 
+ * if briyani price less than 300 taka
+ *      i will buy
+ * 
+ * 
+ * _------STRUCTURE--------
+ * if (condition){
+ *  
+ * }
+ */
+
+// if (3 < 10){
+//     console.log('I have a smaller value');
+// }
+
+// var briyaniPrice = 250;
+
+// if (briyaniPrice < 300){
+//     console.log('mama i want briyani')
+// }
+
+/**
+ * if (condition){
+ *      execute code if condition above is true
+ * }
+ * else {
+ *      execute code if the condition is false
+ * }
+ */
+
+var weight = 20;
+
+if (weight <= 20){
+    console.log('i will carry')
+}
+else {
+    console.log('rent a car')
+}
