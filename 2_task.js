7787b5253278a2303bdb201f60fcd11c755f2fc1
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-29 20:10:02 +0600

    second problem solved

---
+++ 2_task.js
@@ -0,0 +1,20 @@
+/**
+ * Task-2
+Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+Input:
+The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+Output:
+Print the result in 2 decimal places.
+ */
+
+var maths = 75.5;
+var biology = 75.5;
+var chemistry = 80;
+var physics = 90;
+var bangla = 75.5;
+
+var total = maths + biology + chemistry + physics + bangla
+
+console.log(total.toFixed(2))
