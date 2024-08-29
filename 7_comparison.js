0458911143a9548945e3402302955064ed2ffaa3
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-29 22:07:40 +0600

    comparisons

--- 7_comparison.js
+++ 7_comparison.js
@@ -1,1 +1,27 @@
-console.log(5 > 10);
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+
+// console.log(10==10);
+// console.log(10==10.1);
+// ------------
+
+// var peyaraPrice = 40;
+// var applePrice = 250;
+
+// console.log(peyaraPrice > applePrice);
+
+
+/**
+ * ==
+ * === : value r type duitai check kore
+ * 
+ */
+
+console.log(10 == '10');
+console.log(10 === '10');
