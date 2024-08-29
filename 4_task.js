d858f42160530c1134240f5448957cd319294657
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-29 20:16:41 +0600

    fourth problem solved

---
+++ 4_task.js
@@ -0,0 +1,19 @@
+/**
+ * What will be the result of the following codes:
+
+var a = isNaN(‘11’);
+
+var a = isNaN(2-10);
+
+Explain your answers.
+ */
+
+var a = isNaN('11');
+
+var b = isNaN(2-10);
+
+console.log(a)
+console.log(b)
+
+console.log(typeof a)
+console.log(typeof b)
