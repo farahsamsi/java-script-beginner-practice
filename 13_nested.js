186e1c37043bb0a66d8f906e5fe18e98de17d52d
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-30 13:15:34 +0600

    nested else if

---
+++ 13_nested.js
@@ -0,0 +1,18 @@
+const money = 00;
+
+if (money > 300){
+    console.log('rich bro');
+}
+else{
+    if(money > 100){
+        console.log('tui aktu gorib');
+    }
+    else{
+        if (money > 0){
+            console.log('gorib');
+        }
+        else{
+            console.log('no friend');
+        }
+    }
+}
