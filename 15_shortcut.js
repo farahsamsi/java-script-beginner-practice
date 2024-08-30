317394e0621083528173bce2d534c4f89d580816
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-30 13:58:55 +0600

    logical NOT

---
+++ 15_shortcut.js
@@ -0,0 +1,17 @@
+const isLeader = true; 
+// if(isLeader){
+//     console.log('Leader is here');
+// }
+// else{
+//     console.log('Do not let enter');
+// }
+
+const isPassed = true;
+// logical NOT !
+// if(isPassed === false)
+if(!isPassed){
+    console.log('marriage');
+}
+else{
+    console.log('no marriage')
+}
