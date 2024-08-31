d5dc479be09f39ff1089bf50e06a3a2ed5a17b25
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 14:08:41 +0600

    array pop

---
+++ 21_arrayPop.js
@@ -0,0 +1,17 @@
+const friends = ['balam', 'kalam', 'salam', 'pailam', 'gelam'];
+
+// friends.push('gelam');
+console.log(friends);
+
+// pop Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified
+let out1 = friends.pop();
+// console.log(friends);
+let out2 = friends.pop();
+// console.log(friends);
+
+console.log(out1, out2);
+
+
+
+
+ 
