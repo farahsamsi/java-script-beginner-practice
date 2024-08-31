de4f1651bf9baa3bba6f4824bb5fa9c6f8ea576c
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 14:32:24 +0600

    indexof and isArray intro

---
+++ 23_indexof.js
@@ -0,0 +1,16 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam', 'khailam'];
+
+console.log(friends);
+// joins 
+console.log(friends.join('+'));
+
+// array index -1 mane o array te nai
+
+let nums = [];
+let people = 'karim';
+let bool = 'true';
+
+// checks whether a variable is array or not .. TRUE or FALSE returns
+// console.log(Array.isArray(nums));
+// console.log(Array.isArray(people));
+// console.log(Array.isArray(bool));
