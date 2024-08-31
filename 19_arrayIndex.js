da1a0bfb77d4cdc2769085d40826321408a243ed
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 12:07:01 +0600

    array index

---
+++ 19_arrayIndex.js
@@ -0,0 +1,22 @@
+// array indexing starts with 0
+const numbers = [77, 56, 77, 56, 77, 56];
+
+console.log(numbers);
+
+// get element value by index
+console.log(numbers[2]);
+
+
+// assigning an array value to a variable using index
+const fourth = numbers[2];
+console.log(fourth);
+
+// set or update element value by index
+let abc=45;
+abc = 99;
+numbers[1] = 999
+console.log(numbers);
+
+
+
+
