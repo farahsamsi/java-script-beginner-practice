6842e2520182cfd7c7eb61f09707d5e7b0843e04
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-08-31 11:40:41 +0600

    array intro

--- 15_shortcut.js
+++ 15_shortcut.js
@@ -1,4 +1,4 @@
-const isLeader = true; 
+// const isLeader = true; 
 // if(isLeader){
 //     console.log('Leader is here');
 // }
@@ -6,12 +6,34 @@ const isLeader = true;
 //     console.log('Do not let enter');
 // }
 
-const isPassed = true;
-// logical NOT !
-// if(isPassed === false)
-if(!isPassed){
-    console.log('marriage');
-}
-else{
-    console.log('no marriage')
+// const isPassed = true;
+// // logical NOT !
+// // if(isPassed === false)
+// if(!isPassed){
+//     console.log('marriage');
+// }
+// else{
+//     console.log('no marriage')
+// }
+
+// let result = 10 < "5";
+// console.log(result);
+// console.log(6 != 6);
+
+// let x = 5; 
+// if (x < 10) { 
+//     x = x + 3; 
+// } 
+// else { 
+//     x = x - 2; 
+// } 
+// console.log(x);
+
+var num = 10;
+if (num > 15) {
+  console.log("Greater than 15");
+} else if (num > 5) {
+  console.log("Greater than 5 but less than or equal to 15");
+} else {
+  console.log("Less than or equal to 5");
 }
---
+++ 18_arrayLength.js
@@ -0,0 +1,3 @@
+let numbers = [1, 4, 5, 77, 56, 77, 56];
+
+console.log(numbers.length);
