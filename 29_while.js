966f7af13e875ae597c3545a901cab2c3b33143d
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-02 03:47:24 +0600

    while loop

---
+++ 29_while.js
@@ -0,0 +1,16 @@
+const number = 6;
+if (number<1){
+    console.log('small number');
+}
+console.log('something is cooking')
+
+// ---while loop----
+let num= 0; 
+while(num<5){
+    console.log('looooping', num);
+    num = num+1;
+    //loop variable
+    // 1. num = num+1;
+    // 2. num += 1;
+    // 3. num ++ ;
+}
