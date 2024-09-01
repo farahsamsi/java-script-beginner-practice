c9106d64522300f732a3adfa8df7143ae9d1bfed
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-01 22:33:25 +0600

    maximum of three numbers

---
+++ 24_max.js
@@ -0,0 +1,17 @@
+let num1 = 20, num2 = -50, num3 = 25, max;
+
+console.log('num1', num1);
+console.log('num2', num2);
+console.log('num3', num3);
+
+if (num1 >= num2 && num1 >= num3) {
+    console.log(num1, 'is max');
+}
+else if (num2 >= num1 && num2 >= num3) {
+    console.log(num2, 'is max');
+}
+else {
+    console.log(num3, 'is max');
+}
+
+
