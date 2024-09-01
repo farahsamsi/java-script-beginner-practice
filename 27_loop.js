023c67e934cfe58e9e2a1929509dd42b5b0a8242
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-02 03:29:29 +0600

    for of loop

---
+++ 27_loop.js
@@ -0,0 +1,13 @@
+const numbers= [12, 43, 564, 76, 85];
+
+console.log(numbers);
+
+for(const num of numbers){
+    console.log(num);
+}
+
+const fruits = ['orange', 'apple', 'banana'];
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('eat');
+}
---
+++ 28_theory.js
@@ -0,0 +1,17 @@
+// const : value will not change
+// let : value can change
+// var : we should not use
+
+// varriable
+const amount = 299;
+
+// condition
+if (amount> 100){
+
+}
+else{
+
+}
+
+// array 
+const numbers=[12, 24, 564, 32];
