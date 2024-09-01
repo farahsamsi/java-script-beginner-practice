5da81a2e16918e78087ef0054ab63ec9f5319742
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-02 05:44:46 +0600

    range,sum and even

---
+++ 30_range.js
@@ -0,0 +1,16 @@
+// let num = 1;
+
+// while (num<10){
+//     console.log(num);
+//     num++;
+// }
+
+// sum
+let num=1;
+let sum=0;
+while(num<=10){
+    console.log(num);
+    sum= sum+num;
+    console.log('sum:', sum);
+    num++;
+}
