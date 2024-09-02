ebcff77d3305f16625905b4f793dc3d3776b8b0c
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-02 13:31:43 +0600

    break, continue and do while loop

--- 28_theory.js
+++ 28_theory.js
@@ -23,8 +23,27 @@ while(num<3) {   //loop condition
     num++;  //loop variable change
 }
 
+
+// do while loop --> at least 1bar output dekhabei
+let n=3;
+do{
+    console.log(n);
+    n++;
+}
+while(n<=5)
+
 // for loop
 // for( loop variable declaration ; condition ; change)
 for(let num= 0; num<5 ; num++){
     console.log(num);
 }
+
+// continue --> skip this one
+// continue-->skip rest of the code for this iteration
+for (let i=1 ; i<10 ; i++){
+    if(i%2===1){
+        continue;
+    }
+    console.log(i);
+}
+// break --> i am done with this loop, loop end
---
+++ 33_break.js
@@ -0,0 +1,35 @@
+// -------BREAK------
+
+// for (let i=0 ; i<15 ; i++){
+//     console.log(i);
+//     if(i>=5){
+//         break; //condition true hoile break kaaj korbe
+//     }
+// }
+
+// let num=54;
+// while(num>=20){
+//     console.log(num);
+//     if(num<50){
+//         break;
+//     }
+//     num--;
+// }
+
+
+// -----CONTINUE-------
+// continue-->skip rest of the code for this iteration
+// for (let i=1 ; i<10 ; i++){
+//     if(i%2===1){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// do while loop --> at least 1bar output dekhabei
+let n=66;
+do{
+    console.log(n);
+    n++;
+}
+while(n<=5)
