40c6f0cadb45396e0c4ac06d1c8b5ffc502fa084
farahsamsi <1827034farahsamsiprome@gmail.com>
2024-09-02 06:02:38 +0600

    for loop

--- 28_theory.js
+++ 28_theory.js
@@ -15,3 +15,16 @@ else{
 
 // array 
 const numbers=[12, 24, 564, 32];
+
+//while loop
+let num=0; //loop variable
+while(num<3) {   //loop condition
+    console.log(num); //loop statement
+    num++;  //loop variable change
+}
+
+// for loop
+// for( loop variable declaration ; condition ; change)
+for(let num= 0; num<5 ; num++){
+    console.log(num);
+}
---
+++ 32_for.js
@@ -0,0 +1,23 @@
+// for loop
+// for( loop variable declaration ; condition ; change)
+// loop variable declaration sudhu first time korbe
+
+// for(let num= 0; num<5 ; num++){
+//     console.log(num);
+// }
+
+
+// ------odd and even-----
+// for(let i = 1 ; i<=10 ; i++){
+//     if(i%2===0){
+//     console.log(i, 'is even');
+//     }
+//     else{
+//     console.log(i, 'is odd');
+//     }
+// }
+
+// ----even
+for(let i=0 ; i<=10 ; i=i+2){
+    console.log(i);
+}
